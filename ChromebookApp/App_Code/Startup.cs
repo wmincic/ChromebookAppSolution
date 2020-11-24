e8e81fc509d9fbadeced21c1e9e0c29cf88ea019
DESKTOP-A1FTHJ8\wminc <wmincic@gmail.com>
2020-11-23 22:08:19 -0600

    Add project files.

---
+++ ChromebookApp/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChromebookApp.Startup))]
+namespace ChromebookApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
